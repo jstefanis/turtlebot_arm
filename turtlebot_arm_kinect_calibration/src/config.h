3dfb7388d7857a3711caf5690096ba593a40b89b
Giannis Tsagatakis <jtsagata@gmail.com>
2018-12-10 16:25:00 +0100

    Adaptable calibrations

---
+++ turtlebot_arm_kinect_calibration/src/config.h
@@ -0,0 +1,6 @@
+
+#define PROJECT_SOURCE_PATH "/home/talos/ros/phantomX/src/turtlebot_arm/turtlebot_arm_kinect_calibration"
+#define PROJECT_SOURCE_LAUNCHER_PATH "/home/talos/ros/phantomX/src/turtlebot_arm/turtlebot_arm_kinect_calibration/launch"
+
+#define  XML_PARAMS_FILE "calibration_properties_calibrated.xml"
+#define  LAUNCHER_FILENAME "transformation_calibrated.launch"
